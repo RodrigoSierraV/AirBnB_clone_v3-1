3f629c1b406c01b139b9f419beec1996a08d5c3c
RodrigoSierraV <735@holbertonschool.com>
2019-09-30 03:58:07 +0000

    [Add] Listen for changes on each INPUT checkbox tag

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+let amen_Checked = {};
+$(function () {
+  $('input[type=checkbox]').click(function () {
+    if (this.checked) {
+      amen_Checked[this.dataset.id] = this.dataset.name;
+    } else {
+      delete amenitiesChecked[this.dataset.id];
+    }
+    $('.amenities h4').text(Object.values(amen_Checked).join(', '));
+  });
+});
